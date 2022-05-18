678097fa754288f146e87ad3e64cb497fd70d994
차요셉 <jscha@raoncorp.com>
2022-05-18 20:21:55 +0900

    Firebase Realtime Database 연결

--- CreditCardList/CardListViewController.swift
+++ CreditCardList/CardListViewController.swift
@@ -7,16 +7,38 @@
 
 import UIKit
 import Kingfisher
+import FirebaseDatabase
 
 class CardListViewController: UITableViewController {
+    var ref: DatabaseReference! //Firebase Realtime Database
+    
     var creditCardList: [CreditCard] = []
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // UITableView Cell Register
         let nibName = UINib(nibName: "CardListCell", bundle: nil)
         tableView.register(nibName, forCellReuseIdentifier: "CardListCell")
+        
+        ref = Database.database().reference()
+        
+        ref.observe(.value) { snapshot in
+            guard let value = snapshot.value as? [String: [String: Any]] else { return }
+            
+            do {
+                let jsonData = try JSONSerialization.data(withJSONObject: value)
+                let cardData = try JSONDecoder().decode([String: CreditCard].self, from: jsonData)
+                let cardList = Array(cardData.values)
+                self.creditCardList = cardList.sorted { $0.rank < $1.rank }
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            } catch let error {
+                print("ERROR JSON parsing \(error.localizedDescription)")
+            }
+        }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -26,17 +48,16 @@ class CardListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "CardListCell", for: indexPath) as? CardListCell else { return UITableViewCell() }
         
-        cell.rankLabel.text = "\(creditCardList[indexPath.row].rank)"
-        cell.promotionLabel.text = "\(creditCardList[indexPath.row].promotionDetail.amount)만원 증정"
-        cell.cardNameLabel.text = "\(creditCardList[indexPath.row].name)"
-        
         let imageURL = URL(string: creditCardList[indexPath.row].cardImageURL)
         cell.cardImageView.kf.setImage(with: imageURL)
+        cell.rankLabel.text = "\(creditCardList[indexPath.row].rank)위"
+        cell.promotionLabel.text = "\(creditCardList[indexPath.row].promotionDetail.amount)만원 증정"
+        cell.cardNameLabel.text = "\(creditCardList[indexPath.row].name)"
         
         return cell
     }
     
-    override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 80
     }
     
@@ -47,5 +68,39 @@ class CardListViewController: UITableViewController {
         
         detailViewController.promotionDetail = creditCardList[indexPath.row].promotionDetail
         self.show(detailViewController, sender: nil)
+        
+        //Option1
+        let cardID = creditCardList[indexPath.row].id
+        //        ref.child("Item\(cardID)/isSelected").setValue(true)
+        
+        //Option2
+        ref.queryOrdered(byChild: "id").queryEqual(toValue: cardID).observe(.value) { [weak self] snapshot in
+            guard let self = self,
+                  let value = snapshot.value as? [String: [String: Any]],
+                  let key = value.keys.first else { return }
+            
+            self.ref.child("\(key)/isSelected").setValue(true)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            //Option1
+            let cardID = creditCardList[indexPath.row].id
+            ref.child("Item\(cardID)").removeValue()
+            
+//            Option2
+//                        ref.queryOrdered(byChild: "id").queryEqual(toValue: cardID).observe(.value) { [weak self] snapshot in
+//                            guard let self = self,
+//                                  let value = snapshot.value as? [String: [String: Any]],
+//                                  let key = value.keys.first else { return }
+//
+//                            self.ref.child(key).removeValue()
+        }
     }
 }
+
